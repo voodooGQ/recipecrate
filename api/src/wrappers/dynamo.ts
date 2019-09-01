ba04dcd53b55ca191f3f26ec53bc9cd39c45d9c2
Shane Smith <voodoogq@gmail.com>
2019-09-01 10:00:17 -0500

    Creating localized/invokable add function to make the database adds easier

--- api/src/wrappers/dynamo.ts
+++ api/src/wrappers/dynamo.ts
@@ -1,7 +1,6 @@
 import { Logger } from '@sailplane/logger';
 import { DynamoDB } from 'aws-sdk';
 import { Converter } from 'aws-sdk/clients/dynamodb';
-import { CreateAccountStatus } from 'aws-sdk/clients/organizations';
 import BaseWrapper from './base';
 
 /**
@@ -27,5 +26,35 @@ export default class DynamoDBWrapper extends BaseWrapper {
       (data: any) => data.Items.map((item: any) => Converter.unmarshall(item)),
       (err: any) => { throw this.defaultError(err); },
     );
+  }
+
+  public async addRecipe(recipe: any) {
+    if (!recipe.Title) { throw this.defaultError('A title for the recipe must be supplied'); }
+    recipe.Slug = this.slugify(recipe.Title);
+
+    const params = {
+      Item: Converter.marshall(recipe),
+      TableName: this.table,
+    };
+
+    return this.service.putItem(params).promise().then(
+      (data: any) => data,
+      (err: any) => { throw this.defaultError(err); },
+    );
+  }
+
+  private slugify(str: string) {
+    const a = 'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;';
+    const b = 'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------';
+    const p = new RegExp(a.split('').join('|'), 'g');
+
+    return str.toString().toLowerCase()
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(p, (c) => b.charAt(a.indexOf(c))) // Replace special characters
+      .replace(/&/g, '-and-') // Replace & with 'and'
+      .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+      .replace(/^-+/, '') // Trim - from start of text
+      .replace(/-+$/, ''); // Trim - from end of text
   }
 }
