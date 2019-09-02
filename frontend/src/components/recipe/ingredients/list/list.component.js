8a007c57310dad610778ff68338aef2807160036
Shane Smith <voodoogq@gmail.com>
2019-09-02 14:38:16 -0500

    Ingredient List Component

--- frontend/src/components/recipe/ingredients/list/list.component.js
+++ frontend/src/components/recipe/ingredients/list/list.component.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default class IngredientsList extends React.component {
+export default class IngredientsList extends Component {
+  static propTypes = {
+    ingredients: PropTypes.array.isRequired,
+  }
 
-};
+  render() {
+    return (
+      <ul className="recipe-ingredients-list">
+        { this.props.ingredients.map((ingredient, key) => (<li key={key}>{ingredient}</li>)) }
+      </ul>
+    );
+  }
+}
--- frontend/src/components/recipe/ingredients/section/section.component.js
+++ frontend/src/components/recipe/ingredients/section/section.component.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import IngredientsList from '../list/list.component';
 
 export default class IngredientsSection extends Component {
-  state = { ingredients: null }
-
   static propTypes = {
     title: PropTypes.string.isRequired,
     ingredients: PropTypes.array.isRequired,
   }
 
   render() {
-    console.log('In Component');
     return (
       <div className="recipe-ingredients-section">
         <h1>{ this.props.title }</h1>
+        <IngredientsList ingredients={ this.props.ingredients }></IngredientsList>
       </div>
     );
   }
