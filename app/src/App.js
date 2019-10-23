6a9a2ca43a50d484836e405bd4ef7c2a2a2b857c
Shane Smith <voodoogq@gmail.com>
2019-10-23 09:14:20 -0500

    Browser History path replacement

--- app/src/App.js
+++ app/src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './components/pages/home.component';
 import RecipePage from './components/pages/recipe.component';
+import { createBrowserHistory } from 'history';
 
 import './styles/app.scss';
+
+const history = createBrowserHistory();
+const path = (/#!(\/.*)$/.exec(window.location.hash) || [])[1];
+if (path) { history.replace(path); }
 
 export default class App extends Component {
   constructor(props) {
